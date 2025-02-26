07d98b2b674b824cd11a5a2e7d0b200d3e4e452c
jce <jce@melexis.com>
2025-02-26 15:01:39 +0100

    Use report_warning instead of print; don't combine regexes into a single pattern for robustness; rename config variable

--- sphinxcontrib/doxylink/__init__.py
+++ sphinxcontrib/doxylink/__init__.py
@@ -4,7 +4,7 @@ def setup(app):
     from .doxylink import setup_doxylink_roles
     app.add_config_value('doxylink', {}, 'env')
     app.add_config_value('doxylink_pdf_files', {}, 'env')
-    app.add_config_value('doxylink_parse_exception_ignore_regex_list',
+    app.add_config_value('doxylink_parse_error_ignore_regexes',
                          default=[], types=[str], rebuild='env')
     app.connect('builder-inited', setup_doxylink_roles)
 
